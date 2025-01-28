340d7d7a7ee66719207a3efa6e8a28617a151142
Daebreak-II <70728190+Daebreak-II@users.noreply.github.com>
2025-01-27 21:09:12 -0800

    Created Game Scene
    - created 3 canvases for separate rendering
    - renamed test scenes to game and menu scene

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public void StartGame()
-    {
+    [SerializeField] private string MenuSceneName;
+    [SerializeField] private string GameSceneName;
 
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(GameSceneName);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(MenuSceneName);
     }
 
     public void QuitGame()
     {
-
+        Debug.Log("Quitting Game");
+        Application.Quit();
     }
 }
