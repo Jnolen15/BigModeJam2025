c02803294d631f294745c2eb6daadb362372dd1b
KapeRay <67844957+KapeRay@users.noreply.github.com>
2025-01-29 21:29:18 -0800

    Streamer MOves mouth

--- Assets/Scripts/StreamerDialogueManager.cs
+++ Assets/Scripts/StreamerDialogueManager.cs
@@ -1,17 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class StreamerDialogueManager : MonoBehaviour
 {
     [Header("References")]
+    public GameObject Streamer;
     public GameObject DialogueBox;
     public GameObject DialogueTextBox;
 
 
     private TextMeshProUGUI _textMessaage;
+    private Sprite _image;
 
 
     [SerializeField] private List<StreamerTalkTopicSO> _streamerTalkTopicList = new List<StreamerTalkTopicSO>();
@@ -19,6 +25,7 @@ public class StreamerDialogueManager : MonoBehaviour
     void Start()
     {
         _textMessaage = DialogueTextBox.GetComponent<TextMeshProUGUI>();
+        _image = Streamer.GetComponent<UnityEngine.UI.Image>().overrideSprite;
         StartCoroutine("showDialogue");
 
     }
@@ -39,16 +46,22 @@ public class StreamerDialogueManager : MonoBehaviour
         
         while (true)
         {
+            Streamer.GetComponent<UnityEngine.UI.Image>().sprite = _image;
             int _randNumber = Random.Range(3, 10);
             int randTopic = Random.Range(0, _streamerTalkTopicList.Count);
             yield return new WaitForSeconds(_randNumber);
             //Use Scriptable Object here
             _textMessaage.text = _streamerTalkTopicList[randTopic].StreamerDialogue;
+            
+            Streamer.GetComponent<PlayableDirector>().enabled = true;
             DialogueBox.SetActive(true);
 
             yield return new WaitForSeconds(5);
-
+            Streamer.GetComponent<PlayableDirector>().initialTime = 0;
+            Streamer.GetComponent<PlayableDirector>().enabled = false;
             DialogueBox.SetActive(false);
+            
+            
             yield return null;
         }
     }
